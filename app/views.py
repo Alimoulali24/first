633e879a21684637140c3f313d9ca8afa1b4027d
Alimoulali24 <alimoulalidudekula06@gmail.com>
2023-05-20 13:07:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def srujana(request):
+    return HttpResponse('<marquee><h1>srujana thinavara</h1></marquee>')
+def ammulu(request):
+    return HttpResponse('<marquee><h1>sister suresh ki call cheyi</h1></marquee>')
+
+
+
+    
